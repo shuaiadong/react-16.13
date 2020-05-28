ce1da44c07a7d0c9d5a586f02da54f3e034f57e4
zhonghuidong <zhonghuidong@baidu.com>
2020-05-28 10:24:44 +0800

    fix: ReactElement 写错的地方

--- src/react/ReactElement.js
+++ src/react/ReactElement.js
@@ -1,4 +1,4 @@
-import { REACT_ELEMENT_TYPE } from "../shared/ReactSymbols";
+import { REACT_ELEMENT_TYPE , REACT_ELEMENT_TEXT} from "../shared/ReactSymbols";
 
 // utils
 const hasOwnProperty = Object.hasOwnProperty;
@@ -31,8 +31,8 @@ export function createElement(type, config, children) {
     let key = null;
     let ref = null;
 
-    
-    if(config !== null || config !== undefined) {
+
+    if (config !== null || config !== undefined) {
 
 // 1. 处理 ref
         if (hasValidRef(config)) {
@@ -44,7 +44,7 @@ export function createElement(type, config, children) {
             key = config.key;
         }
 
-// 2. 处理 props
+// 3. 处理 props
         for (propName in config) {
             if (
                 hasOwnProperty.call(config, propName)
@@ -55,23 +55,24 @@ export function createElement(type, config, children) {
         }
     }
 
-// 3. children
+// 4. children
     const childrenLength = arguments.length - 2;
-    if(childrenLength === 1) {
-        props.children = children;
+    if (childrenLength === 1) {
+        props.children = [children];
     } else if (childrenLength > 1) {
-        const childrenArray = Array(childrenArray);
+        // **  Array(childrenLength) **
+        const childrenArray = Array(childrenLength);
         for (let i = 0; i < childrenLength; i++) {
             childrenArray[i] = arguments[i + 2];
         }
         props.children = childrenArray;
     }
 
-// 4. defaultProps
+// 5. defaultProps
     if (type && type.defaultProps) {
         const defaultProps = type.defaultProps;
         for (propName in defaultProps) {
-            if(props[propName] === undefined) {
+            if (props[propName] === undefined) {
                 props[props] = defaultProps[propName];
             }
         }
@@ -98,7 +99,7 @@ function ReactElement(type, key, ref, props) {
     };
     return element;
 }
-// ---------------- es6 版
+// ---------------- 简版 实现同样的功能
 // export function createElement(type, config = {}, ...children) {
 //     const {
 //         ref = null,
@@ -107,6 +108,8 @@ function ReactElement(type, key, ref, props) {
 //     } = config;
 
 //     props.children = children.length === 1 ? children[0] : children;
+
+//     **只有 class 组件才有 defaultProps**
 //     if (type && type.defaultProps) {
 //         const defaultProps = type.defaultProps;
 //         for (let propName in defaultProps) {
--- src/shared/ReactSymbols.js
+++ src/shared/ReactSymbols.js
@@ -1,1 +1,13 @@
 export const REACT_ELEMENT_TYPE = Symbol.for('react.element'); // react.element
+export const REACT_ELEMENT_TEXT = Symbol.for('react.text');
+
+// 根fiber
+export const TAG_ROOT = Symbol.for('tag.root');
+// 原生节点
+export const TAG_HOST = Symbol.for('tag.host');
+// 文本
+export const TAG_TEXT = Symbol.for('tag.text');
+
+export const PLACEMENT = Symbol.for('PLACEMENT');
+export const UPDATE = Symbol.for('UPDATE');
+export const DELETE = Symbol.for('DELETE');
